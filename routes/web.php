a901d6ec20c176ee4ef6a10523fbfaaf2aedd3fd
mikewap07 <66952416+mikewap07@users.noreply.github.com>
2020-06-16 11:52:44 +0800

    Registering GET user request
    used Git web GUI

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/users', 'Users@index');
+Route::get('/users/{id}', 'Users@showUser');
