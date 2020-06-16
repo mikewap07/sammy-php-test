2144d5993283acde5d573eff2fa099ee646c8a9f
mikewap07 <66952416+mikewap07@users.noreply.github.com>
2020-06-16 16:14:16 +0800

    Created Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+        	'id' => '1',
+        	'name' => 'John Smith',
+        	'comments' => 'Director',
+
+        ]);
+
     }
 }
